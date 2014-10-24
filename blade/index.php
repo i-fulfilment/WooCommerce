71fc48c62e5256ae3fbdd5db3cc7c6d5a111de4e
Edward Marriner <edmarriner@gmail.com>
2014-10-24 17:06:08 +0100

    Update index.php

--- blade/index.php
+++ blade/index.php
@@ -5,7 +5,7 @@
  * Description: Enables the WooCommerce I-Fulfilment integration with Blade IMS.
  * Author: I-Fulfilment - Edward Marriner - support.team@i-fulfilment.co.uk
  * Author URI: http://www.i-fulfilment.co.uk/
- * Version: 1.1
+ * Version: 1.0
  * Text Domain: blade-fulfilment
  *
  * Copyright: (c) 2014 I-Fulfilment, Inc.
@@ -24,7 +24,7 @@ function create_new_order_statuses() {
         'exclude_from_search'       => false,
         'show_in_admin_all_list'    => true,
         'show_in_admin_status_list' => true,
-        'label_count'               => _n_noop( 'Being processed by Blade <span class="count">(%$
+        'label_count'               => _n_noop( 'Being processed by Blade <span class="count">(%s)</span>', 'Being processed by Blade <span class="count">(%s)</span>' )
     ) );
 
     // Create the awaiting picking status
@@ -34,7 +34,7 @@ function create_new_order_statuses() {
         'exclude_from_search'       => false,
         'show_in_admin_all_list'    => true,
         'show_in_admin_status_list' => true,
-        'label_count'               => _n_noop( 'Awaiting Picking <span class="count">(%s)</span$
+        'label_count'               => _n_noop( 'Awaiting Picking <span class="count">(%s)</span>', 'Awaiting Picking <span class="count">(%s)</span>' )
     ) );
 
     // Create the packed status
@@ -44,9 +44,9 @@ function create_new_order_statuses() {
         'exclude_from_search'       => false,
         'show_in_admin_all_list'    => true,
         'show_in_admin_status_list' => true,
-        'label_count'               => _n_noop( 'Packed, Awaiting Despatch <span class="count">($
-    ) );
-
+        'label_count'               => _n_noop( 'Packed, Awaiting Despatch <span class="count">(%s)</span>', 'Packed, Awaiting Despatch <span class="count">(%s)</span>' )
+    ) );    
+    
     // Create the packed status
     register_post_status( 'wc-blade-despatched', array(
         'label'                     => 'Despatched',
@@ -54,7 +54,7 @@ function create_new_order_statuses() {
         'exclude_from_search'       => false,
         'show_in_admin_all_list'    => true,
         'show_in_admin_status_list' => true,
-        'label_count'               => _n_noop( 'Despatched <span class="count">(%s)</span>', 'D$
+        'label_count'               => _n_noop( 'Despatched <span class="count">(%s)</span>', 'Despatched <span class="count">(%s)</span>' )
     ) );
 }
 
@@ -70,15 +70,15 @@ function load_new_order_statuses( $order_statuses ) {
 
         $new_order_statuses[ $key ] = $status;
 
-        // Rename the default order status 'Processing' to say 'Waiting for fulfilment' to make $
+        // Rename the default order status 'Processing' to say 'Waiting for fulfilment' to make it clearer.
         if ( 'wc-processing' === $key && $status == 'Processing') {
 
                  $new_order_statuses['wc-processing'] = 'Waiting For Fulfilment';
 
                 // Add the new order statuses
-                 $new_order_statuses['wc-blade-processing']              = 'Pulling Order Into B$
-                 $new_order_statuses['wc-blade-picking']                 = 'Items Awaiting Picki$
-                 $new_order_statuses['wc-blade-packed']                  = 'Items Packed, Awaiti$
+                 $new_order_statuses['wc-blade-processing']              = 'Pulling Order Into Blade IMS';
+                 $new_order_statuses['wc-blade-picking']                 = 'Items Awaiting Picking';
+                 $new_order_statuses['wc-blade-packed']                  = 'Items Packed, Awaiting Order Despatch';
                  $new_order_statuses['wc-blade-despatched']              = 'Order Despatched';
         }
     }
